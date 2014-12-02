d99e5ce9a5afcb62ea399046a6bc2f20b27ee880
njdbickhart <njdbickhart@gmail.com>
2014-12-01 21:38:23 -0500

    updated to work with new htslib packages for bcftools

--- sequence_data_scripts/samtoolsSNPFork.pl
+++ sequence_data_scripts/samtoolsSNPFork.pl
@@ -2,13 +2,14 @@
 # This script uses the Forks super package to split up Samtools chromosome processing
 # samtools segments are in UCSC format and are newline separated values: (chr):(start)-(end)
 # Samtools must be in the system $PATH variable in order to run this program
+# Version update: added ability to work with the new htslib bcftools package
 
 use strict;
 use Forks::Super;
 use Getopt::Std;
 
 my %opts;
-my $usage = "perl $0 -r <reference genome fasta> -i <input bams, separated by commas> -o <output bam basename> -n <Max number of threads> -s <segments [overrides default chr processing]>\n";
+my $usage = "perl $0 -r <reference genome fasta> -i <input bams, separated by commas> -o <output bam basename> -n <Max number of threads> -s <segments [overrides default chr processing]> -t <htslib (new samtools) version processing>\n";
 
 getopt('rions', \%opts);
 
@@ -28,6 +29,11 @@ my $reffai = "$opts{r}.fai";
 unless(-e $reffai){
 	print "Generating reference fasta index...\n";
 	system("samtools index $ref");
+}
+
+my $ishts = 0;
+if(defined($opts{'t'})){
+	$ishts = 1;
 }
 
 my @chrs;
@@ -54,13 +60,13 @@ foreach my $c (@chrs){
 	
 	push(@bcfs, $bcf);
 	
-	fork { sub => \&runSamtoolsBCFCaller, args => [$ref, $bcf, $opts{'i'}, $c], max_proc => $opts{'n'} };
+	fork { sub => \&runSamtoolsBCFCaller, args => [$ref, $bcf, $opts{'i'}, $c, $ishts], max_proc => $opts{'n'} };
 }
 
 waitall();
 
 # merge files and call variants
-concatBCFtoVCF(\@bcfs, "$opts{o}.samtools.merged.bcf", "$opts{o}.samtools.filtered.vcf");
+concatBCFtoVCF(\@bcfs, "$opts{o}.samtools.merged.bcf", "$opts{o}.samtools.filtered.vcf", $ishts);
 
 # Check to see if the vcf exists and is not empty, then delete bcfs
 if( -s "$opts{o}.samtools.filtered.vcf"){
@@ -74,19 +80,28 @@ if( -s "$opts{o}.samtools.filtered.vcf"){
 exit;
 
 sub concatBCFtoVCF{
-	my ($bcfs, $mergebcf, $vcf) = @_;
+	my ($bcfs, $mergebcf, $vcf, $ishts) = @_;
 	
 	my $bcfstr = join(' ', @bcfs);
-	system("bcftools cat $bcfstr > $mergebcf");
-	system("bcftools view $mergebcf | vcfutils.pl varFilter -D100 > $vcf");
+	if($ishts){
+		system("bcftools concat -o $mergebcf -O b $bcfstr");
+		system("bcftools filter -O u -o $vcf -s LOWQUAL -i \'%QUAL>10\' $mergebcf");
+	}else{
+		system("bcftools cat $bcfstr > $mergebcf");
+		system("bcftools view $mergebcf | vcfutils.pl varFilter -D100 > $vcf");
+	}
 }
 
 sub runSamtoolsBCFCaller{
-	my ($ref, $bcf, $bamstr, $chr) = @_;
+	my ($ref, $bcf, $bamstr, $chr, $ishts) = @_;
 	my @bams = split(/,/, $bamstr);
 	my $whitebams = join(' ', @bams);
 	
-	system("samtools mpileup -uf $ref -r $chr $whitebams | bcftools view -bvcg - > $bcf"); 
+	if($ishts){
+		system("samtools mpileup -ugf $ref -r $chr $whitebams | bcftools call -vmO b -o $bcf");
+	}else{
+		system("samtools mpileup -uf $ref -r $chr $whitebams | bcftools view -bvcg - > $bcf"); 
+	}
 	
 	print "Finished creating bcf on chr: $chr\n";
 }
