d0b2dbdfa7226aade8f59f13981a1458bf2e817e
njdbickhart <njdbickhart@gmail.com>
2014-12-17 18:12:15 -0500

    Added grep functionality to vcf conversion

--- sequence_data_scripts/annotatedSNPRegionToTabFile.pl
+++ sequence_data_scripts/annotatedSNPRegionToTabFile.pl
@@ -4,73 +4,109 @@
 use strict;
 use Getopt::Std;
 
-my $usage = "perl $0 -b <indexed bcf file> -r <region in ucsc format> -o <output file>\n";
+my $usage = "perl $0 -b <indexed bcf file> (-g <grep string> || -r <region in ucsc format>) -o <output file>\n";
 my %opts;
-getopt('bro', \%opts);
+getopt('brog', \%opts);
 
-unless(defined($opts{'b'}) && defined($opts{'r'}) && defined($opts{'o'})){
+unless(defined($opts{'b'}) && (defined($opts{'g'}) || defined($opts{'r'})) && defined($opts{'o'})){
 	print $usage;
 	exit;
 }
 
-#CHROM  POS     ID      REF     ALT    QUAL     FILTER  INFO    FORMAT  BIBR02.1        BIBR03.1        
-open(IN, "bcftools view $opts{b} $opts{r} |") || die "Could not open bcftools output!\n $usage";
 open(OUT, "> $opts{o}");
+
+# Retrieve header sequence
+my @header = getHeaderInfo($opts{'b'});
+print OUT join("\t", @header) . "\n";
+
+#CHROM  POS     ID      REF     ALT    QUAL     FILTER  INFO    FORMAT  BIBR02.1        BIBR03.1        
+my $in;
+if(defined($opts{'r'})){
+	open($in, "bcftools view $opts{b} $opts{r} |") || die "Could not open bcftools output!\n $usage";
+}elsif(defined($opts{'g'})){
+	open($in, "bcftools view $opts{b} | grep \'$opts{g}\' |") || die "Could not open bcftools output!\n $usage";
+}
+
 #my @header;
-while(my $line = <IN>){
-	if($line =~ /^##/){next;}
-	elsif($line =~/^#CHROM/){
-		chomp $line;
-		my @segs = split(/\s+/, $line);
-		#push(@header, ("CHR", $segs[1], $segs[3], $segs[4], $segs[5], "MUTATION", "PRIORITY", "GENE"));
-		#push(@header, @segs[9 .. $#segs]);
-		print OUT "CHR\t$segs[1]\t$segs[3]\t$segs[4]\t$segs[5]\tTYPE\tMUTATION\tPRIORITY\tGENE\tAA\t";
-		print OUT join("\t", @segs[9 .. $#segs]) . "\n";
-	}else{
-		chomp $line;
-		my @segs = split(/\t/, $line);
-		print OUT "$segs[0]\t$segs[1]\t$segs[3]\t$segs[4]\t$segs[5]\t";
-		#INDEL;IS=4,1.000000;DP=84;VDB=1.008941e-02;AF1=0.9542;AC1=138;DP4=2,2,52,8;MQ=28;FQ=-8.94;PV4=0.11,1,0.19,1;EFF=INTERGENIC(MODIFIER||||||||||1)
-		my @infosegs = split(/;/, $segs[7]);
-		if($infosegs[0] eq "INDEL"){
-			print OUT "INDEL";
-		}else{
-			print OUT "SNP";
-		}
-		
-		#EFF=SYNONYMOUS_CODING(LOW|SILENT|acG/acA|T334|581|PRLR|protein_coding|CODING|ENSBTAT00000014437|9|1)
-		my @annosegs = split(/,/, $infosegs[-1]);
-		my @mutation;
-		my @priority;
-		my @gene;
-		my @aas;
-		my ($m, $p, $g, $aa);
-		foreach my $a (@annosegs){
-			$a =~ s/EFF=//g;
-			($m) = $a =~ /(.+)\(.*\)/;
-			my ($str) = $a =~ /.+\((.*)\)/;
-			my @effsegs = split(/\|/, $str);
-			$p = $effsegs[0];
-			$g = $effsegs[5];
-			$aa = $effsegs[3];
-			push(@aas, $aa);
-			push(@mutation, $m);
-			push(@priority, $p);
-			push(@gene, $g);
-		}
-		
-		print OUT "\t" . join(';', @mutation) . "\t" . join(';', @priority) . "\t" . join(';', @gene) . "\t" . join(';', @aas);
-		
-		for(my $x = 9; $x < scalar(@segs); $x++){
-			my @forsegs = split(/:/, $segs[$x]);
-			my @gsegs = split(/[\/\|]/, $forsegs[0]);
-			
-			print OUT "\t" . join(';', @gsegs);
-		}
-		print OUT "\n";
+while(my $line = <$in>){
+	if($line =~ /^#/){next;}
+	else{
+		my @saved = processVCFLine($line);
+		print OUT join("\t", @saved) . "\n";
 	}
 }
-close IN;
+close $in;
 close OUT;
 
 exit;
+
+sub processVCFLine{
+	my ($line) = @_;
+	chomp $line;
+	my @saved;
+	
+	my @segs = split(/\t/, $line);
+	#print OUT "$segs[0]\t$segs[1]\t$segs[3]\t$segs[4]\t$segs[5]\t";
+	push(@saved, ($segs[0], $segs[1], $segs[3], $segs[4], $segs[5]));
+	#INDEL;IS=4,1.000000;DP=84;VDB=1.008941e-02;AF1=0.9542;AC1=138;DP4=2,2,52,8;MQ=28;FQ=-8.94;PV4=0.11,1,0.19,1;EFF=INTERGENIC(MODIFIER||||||||||1)
+	my @infosegs = split(/;/, $segs[7]);
+	if($infosegs[0] eq "INDEL"){
+		#print OUT "INDEL";
+		push(@saved, "INDEL");
+	}else{
+		#print OUT "SNP";
+		push(@saved, "SNP");
+	}
+
+	#EFF=SYNONYMOUS_CODING(LOW|SILENT|acG/acA|T334|581|PRLR|protein_coding|CODING|ENSBTAT00000014437|9|1)
+	my @annosegs = split(/,/, $infosegs[-1]);
+	my @mutation;
+	my @priority;
+	my @gene;
+	my @aas;
+	my ($m, $p, $g, $aa);
+	foreach my $a (@annosegs){
+		$a =~ s/EFF=//g;
+		($m) = $a =~ /(.+)\(.*\)/;
+		my ($str) = $a =~ /.+\((.*)\)/;
+		my @effsegs = split(/\|/, $str);
+		$p = $effsegs[0];
+		$g = $effsegs[5];
+		$aa = $effsegs[3];
+		push(@aas, $aa);
+		push(@mutation, $m);
+		push(@priority, $p);
+		push(@gene, $g);
+	}
+
+	#print OUT "\t" . join(';', @mutation) . "\t" . join(';', @priority) . "\t" . join(';', @gene) . "\t" . join(';', @aas);
+
+	push(@saved, (join(';', @mutation), join(';', @priority), join(';', @gene), join(';', @aas)));
+	for(my $x = 9; $x < scalar(@segs); $x++){
+		my @forsegs = split(/:/, $segs[$x]);
+		my @gsegs = split(/[\/\|]/, $forsegs[0]);
+
+		#print OUT "\t" . join(';', @gsegs);
+		push(@saved, join(';', @gsegs));
+	}
+	#print OUT "\n";
+	
+	return @saved;
+}
+
+sub getHeaderInfo{
+	my ($file) = @_;
+	open(IN, "bcftools view $file | grep \'#CHROM\' |");
+	my @header;
+	while(my $line = <IN>){
+		chomp $line;
+		my @segs = split(/\s+/, $line);
+		push(@header, ("CHR", $segs[1], $segs[3], $segs[4], $segs[5], "TYPE", "MUTATION", "PRIORITY", "GENE", "AA"));
+		push(@header, @segs[9 .. $#segs]);
+		
+		#print OUT "CHR\t$segs[1]\t$segs[3]\t$segs[4]\t$segs[5]\tTYPE\tMUTATION\tPRIORITY\tGENE\tAA\t";
+		#print OUT join("\t", @segs[9 .. $#segs]) . "\n";
+	}
+	close IN;
+	return @header;
+}
