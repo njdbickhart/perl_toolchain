e3bc37724acee00507ef2910041943f9ce041a59
Derek Bickhart <njdbickhart@gmail.com>
2015-01-29 09:54:11 -0500

    Strange bug with Forks Super install.
    It only seems to work if invoked twice

--- step2_required_library_install.sh
+++ step2_required_library_install.sh
@@ -7,9 +7,14 @@ cpanm namespace::autoclean
 cpanm Mouse
 cpanm Sys::CpuLoadX
 cpanm Sys::CpuAffinity
-cpanm --verbose Forks::Super
+
 cpanm MouseX::NativeTraits
 cpanm Spreadsheet::WriteExcel
 cpanm List::Util
+echo "Attempting Forks Super install..."
+echo "WARNING! This module is stubborn and may need to be installed twice."
+echo "To do this, run '$ cpanm Forks::Super' after the termination of this script."
+cpanm --verbose Forks::Super
+
 
 echo "Finished installation!"
