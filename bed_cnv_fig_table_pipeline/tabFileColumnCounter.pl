b66bb4df88d04f82182a3487e1fb397886f22aa6
njdbickhart <njdbickhart@gmail.com>
2015-05-14 12:43:41 -0400

    multiple file table functionality

--- bed_cnv_fig_table_pipeline/tabFileColumnCounter.pl
+++ bed_cnv_fig_table_pipeline/tabFileColumnCounter.pl
@@ -8,7 +8,7 @@ use Getopt::Std;
 my $usage = "perl $0 [options] -f <input file> -c <column number>
 	This script is designed to process a tab delimited file to generate some basic statistics on column field count.
 REQUIRED:
-	-f	Input space/tab delimited file. Beginning spaces are removed from each line. Trailing spaces are condensed
+	-f	Input space/tab delimited files separated by commas. Beginning spaces are removed from each line. Trailing spaces are condensed
 	-c	The column number (Zero based!) to count features
 	
 OPTIONAL:
@@ -27,18 +27,60 @@ unless(defined($opts{'f'}) && defined($opts{'c'})){
 }
 
 my $mbool = (defined($opts{'m'}))? 1 : 0;
-my $worker = ColumnCounter->new('colnum' => $opts{'c'}, 'mkdwn' => $opts{'m'});
 
-if(defined($opts{'e'})){
-	$worker->ignore($opts{'e'});
+# Determine how many files we're working with here.
+my @files = split(/,/, $opts{'f'});
+if(scalar(@files) == 1){
+	# Just a single comparison
+	my $worker = ColumnCounter->new('colnum' => $opts{'c'}, 'mkdwn' => $mbool);
+	
+	if(defined($opts{'e'})){
+		$worker->ignore($opts{'e'});
+	}
+	
+	if(defined($opts{'o'})){
+		$worker->output($opts{'o'});
+	}
+	$worker->readFile($opts{'f'});
+	$worker->writeOut();
+}else{
+	# Now we have more to work with
+	my $manager = SummaryManager->new('mkdwn' => $mbool);
+	
+	if(defined($opts{'o'})){
+		$manager->output($opts{'o'});
+	}
+	for (my $x = 0; $x < scalar(@files); $x++){
+		my $filename = "File" . ($x + 1);
+		
+		my $worker = ColumnCounter->new('colnum' => $opts{'c'}, 'mkdwn' => $mbool);
+		
+		if(defined($opts{'e'})){
+			$worker->ignore($opts{'e'});
+		}
+		
+		$worker->readFile($files[$x]);
+		$manager->setTable($filename, $worker->createSumTable($filename));
+	}
+	
+	if(defined($opts{'o'})){
+		open(OUT, "> $opts{o}");
+		for(my $x = 0; $x < scalar(@files); $x++){
+			my $filename = "File" . ($x + 1);
+			print OUT "$filename\:  $files[$x]\n";
+		}
+		close OUT;
+	}else{
+		for(my $x = 0; $x < scalar(@files); $x++){
+			my $filename = "File" . ($x + 1);
+			print "$filename\:  $files[$x]\n";
+		}
+	}
+	
+	$manager->PrintResults();
 }
 
-if(defined($opts{'o'})){
-	$worker->output($opts{'o'});
-}
 
-$worker->readFile($opts{'f'});
-$worker->writeOut();
 
 exit;
 
@@ -86,6 +128,23 @@ sub readFile{
 	close IN;
 	$self->counter(\%hash);
 		
+}
+
+sub createSumTable{
+	my ($self, $name) = @_;
+	my $table = CountSummaryTable->new('name' => $name);
+	my @sortedkeys = sort{$a cmp $b} $self->Keys;
+	foreach my $k (@sortedkeys){
+		my $kvalue = $k; 
+		if($k eq ''){
+			$kvalue = "<Null>";
+		}
+		my $count = $self->Get($k);
+
+		$table->addEntry($kvalue);
+		$table->addValue($count);
+	}
+	return $table;
 }
 
 sub writeOut{
@@ -144,5 +203,192 @@ sub writeOut{
 }
 
 __PACKAGE__->meta->make_immutable;
+
+package SummaryManager;
+use Mouse;
+use namespace::autoclean;
+
+# Name => AbsSummaryTable
+has 'tables' => (traits => ['Hash'], is => 'rw', isa => 'HashRef[Any]', default => sub{{}},
+	handles => {
+		'setTable' => 'set', 
+		'getTable' => 'get',
+		'tableKeys' => 'keys',
+	});
+	
+has 'output' => (is => 'rw', isa => 'Str', predicate => 'has_output');
+has 'mkdwn' => (is => 'ro', isa => 'Bool', required => 1);
+
+sub PrintResults{
+	my ($self) = @_;
+	
+	my $fh; 
+	if($self->has_output){
+		my $out = $self->output;
+		open(OUT, ">> $out");
+		$fh = *OUT;
+	}else{
+		$fh = *STDOUT;
+	}
+	my @sortedkeys = sort{$a cmp $b} $self->tableKeys;
+	
+	foreach my $k (@sortedkeys){
+		print {$fh} $self->getTable($k)->formatHeader($self->mkdwn);
+	}
+	if($self->mkdwn){
+		print {$fh} "\|\n";
+	}else{
+		print {$fh} "\n";
+	}
+	
+	if($self->mkdwn){
+		foreach my $k (@sortedkeys){
+			print {$fh} $self->getTable($k)->formatSep($self->mkdwn);
+		}
+		print {$fh} "\|\n";
+	}
+	
+	my @output;
+	my $maxlen = 0;
+	my $blank;
+	foreach my $k (@sortedkeys){
+		if($self->getTable($k)->numEntry > $maxlen){
+			$maxlen = $self->getTable($k)->numEntry;
+		}
+		push(@output, $self->getTable($k)->formatOutput($self->mkdwn));
+		$blank = $self->getTable($k)->formatBlank($self->mkdwn);
+	}
+	
+	for( my $j = 0; $j < $maxlen; $j++){
+		for( my $i = 0; $i < scalar(@output); $i++){
+			if($j > scalar(@{$output[$i]})){
+				print {$fh} $blank;
+			}else{
+				print {$fh} $output[$i]->[$j];
+			}
+		}
+		if($self->mkdwn){
+			print {$fh} "\|\n";
+		}else{
+			print {$fh} "\n";
+		}
+	}
+	print "\n";
+	if($self->has_output){
+		close OUT;
+	}
+}
+
+__PACKAGE__->meta->make_immutable;
+
+package CountSummaryTable;
+use Mouse;
+use namespace::autoclean;
+
+with 'AbsSummaryTable';
+
+has 'entryTab' => (traits => ['Array'], is => 'rw', isa => 'ArrayRef[Str]', default => sub{[]},
+	handles => {
+		'addEntry' => 'push',
+		'getEntry' => 'get',
+		'numEntry' => 'count',
+		'allEntry' => 'elements',
+	});
+has 'valueTab' => (traits => ['Array'], is => 'rw', isa => 'ArrayRef[Int]', default => sub{[]},
+	handles => {
+		'addValue' => 'push',
+		'getValue' => 'get',
+		'numValue' => 'count',
+		'allValue' => 'elements',
+	});
+
+has 'entryLen' => (is => 'rw', isa => 'Int', builder => '_entryLenCount', lazy => 1);
+has 'valueLen' => (is => 'rw', isa => 'Int', builder => '_valueLenCount', lazy => 1);
+
+sub formatOutput{
+	my ($self, $mrkdown) = @_;
+	my @values;
+	if($mrkdown){
+		for(my $x = 0; $x < $self->numEntry; $x++){
+			push(@values, sprintf("\|%-*s\|%*d", $self->entryLen, $self->getEntry($x), $self->valueLen, $self->getValue($x)));
+		}
+	}else{
+		for(my $x = 0; $x < $self->numEntry; $x++){
+			push(@values, $self->getEntry($x) . "\t" . $self->getValue($x) . "\t");
+		}
+	}
+	return \@values;
+}
+
+sub formatBlank{
+	my ($self, $mrkdown) = @_;
+	if($mrkdown){
+		return sprintf("\|%-*s\|%*s", $self->entryLen, " ", $self->valueLen, " ");
+	}else{
+		return "\t\t";
+	}
+}
+
+sub formatHeader{
+	my ($self, $mrkdown) = @_;
+	my $str;
+	if($mrkdown){
+		$str = sprintf("\|%-*s\|%*s", $self->entryLen, $self->name, $self->valueLen, "Count");
+	}else{
+		$str = $self->name . "\t";
+		$str .= "Count\t";
+	}
+	return $str;
+}
+
+sub formatSep{
+	my ($self, $mrkdown) = @_;
+	my $str;
+	if($mrkdown){
+		my $sepstr = '-' x ($self->entryLen - 1);
+		my $sepcon = '-' x ($self->valueLen - 1);
+		$str = sprintf("\|\:%s\|%s\:", $sepstr, $sepcon);
+	}else{
+		$str = "";
+	}
+	return $str;
+}
+
+sub _entryLenCount{
+	my ($self) = @_;
+	my $collen = length($self->name);
+	foreach my $k ($self->allEntry){
+		if(length($k) > $collen){
+			$collen = length($k);
+		}
+	}
+	$collen++;
+	return $collen;
+}
+
+sub _valueLenCount{
+	my ($self) = @_;
+	my $collen = 5;
+	foreach my $k ($self->allValue){
+		if(length($k) > $collen){
+			$collen = length($k);
+		}
+	}
+	$collen++;
+	return $collen;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+package AbsSummaryTable;
+use Mouse::Role;
+use namespace::autoclean;
+
+has 'name' => (is => 'ro', isa => 'Str', required => 1);
+
+requires 'formatOutput';
+requires 'formatHeader';
+requires 'formatSep';
+requires 'formatBlank';
 
 }
