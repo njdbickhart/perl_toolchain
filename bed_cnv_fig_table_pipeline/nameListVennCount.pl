fbce2879af91d2e29a715de2c2fd7254149a8b7d
njdbickhart <njdbickhart@gmail.com>
2015-05-21 16:03:25 -0400

    New function to venn script

--- bed_cnv_fig_table_pipeline/nameListVennCount.pl
+++ bed_cnv_fig_table_pipeline/nameListVennCount.pl
@@ -1,12 +1,19 @@
 #!/usr/bin/perl
 # This script is designed to take two files, identify the unique names, and then peform venn counts
 # I'm going to start off easy at first
+# Adding the ability to print out groups to separate files for later retrieval
 
 use strict;
 
 chomp(@ARGV);
 
-my $usage = "Must enter at least two files\nperl $0 file1 file2 file3 ... fileN\n";
+my $usage = "Must enter at least two files\nperl $0 [-o : flag to print out groupings to text] file1 file2 file3 ... fileN\n";
+
+my $outputflag = 0;
+if($ARGV[0] eq "-o"){
+	$outputflag = 1;
+	shift(@ARGV);
+}
 
 if(scalar(@ARGV) <= 1){
 	print $usage;
@@ -26,6 +33,7 @@ for(my $x = 0; $x < scalar(@ARGV); $x++){
 }
 
 my %counter;
+my %printout;
 foreach my $k (keys(%store)){
 	my $aref = $store{$k};
 	my %set;
@@ -34,12 +42,28 @@ foreach my $k (keys(%store)){
 	}
 	
 	my $group = join(";", sort{ $a <=> $b} keys(%set));
+	if($outputflag){
+		push(@{$printout{$group}}, $k);
+	}
 	$counter{$group} += 1;
 }
 
 print "Set\tCount\n";
 foreach my $k (sort { $a cmp $b} keys(%counter)){
 	print "$k\t$counter{$k}\n";
+}
+
+if($outputflag){
+	print "\n";
+	foreach my $g (keys(%printout)){
+		my $n = $g;
+		$n =~ s/;/_/g;
+		open(OUT, "> group_$n.txt");
+		print OUT join("\n", @{$printout{$g}});
+		print OUT "\n";
+		close OUT;
+		print "Group: $g in output file: group_$n.txt\n";
+	}
 }
 
 exit;
