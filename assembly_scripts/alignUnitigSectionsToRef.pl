9552c32f659d17e083fedcbed3d60bd71cdecfa4
njdbickhart <njdbickhart@gmail.com>
2015-09-10 13:30:15 -0400

    unitig to ref genome alignment script

--- assembly_scripts/alignUnitigSectionsToRef.pl
+++ assembly_scripts/alignUnitigSectionsToRef.pl
@@ -2,6 +2,47 @@
 # This script subsections fastq entries, aligns them to a reference genome and attempts to identify consensus assembly regions
 
 use strict;
+use Getopt::Std;
+
+my $usage = "perl $0 -f <unitig fastq> -r <bwa indexed ref genome> -o <output tab file>\n";
+my %opts;
+getopt('fro', \%opts);
+
+unless(defined($opts{'f'}) && defined($opts{'r'}) && defined($opts{'o'})){
+	print $usage;
+	exit;
+}
+
+my $worker = unitigFastq->new();
+
+open(my $IN, "< $opts{f}") || die "Could not open input: $opts{f}!\n";
+while(my $name = <$IN>){
+	my $seq = <$IN>;
+	my $plus = <$IN>;
+	my $qual = <$IN>;
+	
+	chomp($name, $seq, $qual);
+	$name =~ s/\r//g;
+	$seq =~ s/\r//g;
+	$qual =~ s/\r//g;
+	$worker->loadFastq($name, $seq, $qual);
+}
+close $IN;
+
+print STDERR "loaded fastq file!\n";
+
+$worker->alignAndProcess($opts{'r'});
+
+open(my $OUT, "> $opts{o}");
+my @outputlines = $worker->produceOutput();
+
+foreach my $row (@outputlines){
+	print {$OUT} join("\t", @{$row}) . "\n";
+}
+close $OUT;
+print STDERR "Output in: $opts{o}\n";
+
+exit;
 
 BEGIN{
 package unitigFastq;
@@ -12,7 +53,7 @@ has ['sequence', 'origname', 'quality', 'tempfq'] => (is => 'rw', isa => 'Str');
 has 'unitigMappings' => (is => 'rw', isa => 'ArrayRef[Any]');
 
 sub loadFastq{
-	my ($self, $name, $seq, $plus, $qual) = @_;
+	my ($self, $name, $seq, $qual) = @_;
 	
 	my ($origname) = $name =~ m/\@(.+)$/;
 	$self->origname($origname);
@@ -38,9 +79,59 @@ sub loadFastq{
 sub alignAndProcess{
 	my ($self, $reference) = @_;
 	
+	my @mappings;
 	open(my $IN, "bwa mem $reference temp.fq |"); 
 	while(my $line = <$IN>){
+		if($line =~ /^\@/){next;}
 		
+		chomp $line;
+		my $tempmap = unitigMapping->new();
+		$tempmap->addSamLine($line);
+		push(@mappings, $tempmap);
+	}
+	close $IN;
+	
+	@mappings = sort {$a->ustart <=> $b->ustart} @mappings;
+	
+	$self->unitigMappings(\@mappings);
+}
+
+sub produceOutput{
+	my ($self) = @_;
+	
+	# name	ustart	uend	chr	start	end	qualitystring
+	my @outlines;
+	my $prevchr = "NA"; my $prevustart = 200000000; my $prevcstart = 200000000; my @qualities;
+	my ($prevuend, $prevcend);
+	foreach my $map (@{$self->unitigMappings}){
+		if($map->chr() ne $prevchr && $prevchr ne "NA"){
+			if($prevustart != 200000000 && $prevcstart != 200000000){
+				push(@outlines, [$self->origname, $prevustart, $prevuend, $prevchr, $prevcstart, $prevcend, join(";", @qualities)]);
+			}
+			@qualities = ();
+			push(@qualities, $map->quality);
+			$prevchr = $map->chr();
+			$prevustart = $map->ustart;
+			$prevuend = $map->uend;
+			$prevcstart = $map->start;
+			$prevcend = $map->end;
+		}elsif($prevchr eq "NA"){
+			$prevchr = $map->chr();
+			$prevustart = $map->ustart;
+			$prevuend = $map->uend;
+			$prevcstart = $map->start;
+			$prevcend = $map->end;
+			push(@qualities, $map->quality);
+		}else{
+			$prevuend = $map->uend;
+			$prevcend = $map->end;
+			push(@qualities, $map->quality);
+		}
+	}
+	push(@outlines, [$self->origname, $prevustart, $prevuend, $prevchr, $prevcstart, $prevcend, join(";", @qualities)]);
+	
+	return @outlines;
+}
 
 __PACKAGE__->meta->make_immutable;
 
